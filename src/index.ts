d631a12e1b788349cbf7058712d3479c5ef3820a
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-05-11 22:20:59 -0300

    feat: add AppDataSource to connected data base

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Start server");
+import express from "express";
+import { AppDataSource } from "./data-source";
+
+AppDataSource.initialize().then(() => {
+  const app = express();
+  app.use(express.json());
+
+  const port = process.env.PORT;
+
+  app.get("/", (_, res) => {
+    return res.json(`🔥 Welcome to server, is living port: ${port} 🔥`);
+  });
+
+  return app.listen(port, () =>
+    console.log(`🔥 Start server port = ${port} 🔥`)
+  );
+});
