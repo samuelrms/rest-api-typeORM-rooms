7d49bf82cef6c8defae16d3d3d0c2c3940bd53e1
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-05-14 19:07:10 -0300

    refactor: create video by room

--- src/controllers/VideoController.ts
+++ src/controllers/VideoController.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
+import { roomRepository } from "../repositories/roomRepository";
 import { videoRepository } from "../repositories/videoRepository";
 
 export class VideoController {
   async create(req: Request, res: Response) {
     const { video_title, url } = req.body;
+    const { id_class } = req.params;
 
     if (!video_title)
       return res.status(400).json({ msg: "Video title is required" });
@@ -11,9 +13,16 @@ export class VideoController {
     if (!url) return res.status(400).json({ msg: "URL is required" });
 
     try {
+      const room = await roomRepository.findOneBy({
+        id: id_class as unknown as number,
+      });
+
+      if (!room) return res.status(404).json({ msg: "Room not found" });
+
       const newVideo = videoRepository.create({
         title: video_title,
-        url: url,
+        url,
+        room,
       });
 
       await videoRepository.save(newVideo);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { SubjectController } from "../controllers/SubjectController";
-import { VideoController } from "../controllers/VideoController";
 import { RoomController } from "../controllers/RoomController";
+import { VideoController } from "../controllers/VideoController";
 
 const router = Router();
 
 router.post("/subject", new SubjectController().create);
-router.post("/video", new VideoController().create);
 router.post("/room", new RoomController().create);
+router.post("/room/:id_class/create", new VideoController().create);
 
 export default router;
