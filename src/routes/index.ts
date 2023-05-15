2b4e60451ce790bcc809d261e3e871f69f9eaf32
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-05-15 19:25:06 -0300

    feat: create routes RoomSubject and ListRoom

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 import { SubjectController } from "../controllers/SubjectController";
 import { RoomController } from "../controllers/RoomController";
 import { VideoController } from "../controllers/VideoController";
+import { RoomSubjectController } from "../controllers/RoomSubjectController";
+import { ListRoomController } from "../controllers/ListRoom";
 
 const router = Router();
 
 router.post("/subject", new SubjectController().create);
 router.post("/room", new RoomController().create);
 router.post("/room/:id_class/create", new VideoController().create);
+router.post("/room/:id_class/subject", new RoomSubjectController().create);
+router.get("/room", new ListRoomController().list);
 
 export default router;
